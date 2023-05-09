81b7428cf6feaba955d9357be858c81bb61438dd
Calatin21 <oshaughnessy@hotmail.de>
2023-05-09 09:35:38 +0200

    patch

--- Arrrrh/Program.cs
+++ Arrrrh/Program.cs
@@ -2,20 +2,21 @@
     internal class Program {
         static void Main(string[] args) {
             Meer ozean = new() { Name = "Sieben Meere"};
-            //Insel insel = new() { Name = "Monkey Island" };
-            //Wald wald = new() { Name = "Zick Zack Wald" };
-            //Kneipe kneipe = new() { Name = "Zum bunten Papagei" };
-            //Pirat pirat = new() { Name = "Guybrush" };
-            //ozean.Island = insel;
-            //insel.NaturContainer = wald;
-            //insel.BarContainer = kneipe;
-            //insel.BarContainer.Container = pirat;
-            //insel.ZeigDichPirat();
-            //ozean.SaveBinary();
-            //ozean.SaveXML();
-            //ozean.SaveJson();
-
-
+            Insel insel = new() { Name = "Monkey Island" };
+            Wald wald = new() { Name = "Zick Zack Wald" };
+            Kneipe kneipe = new() { Name = "Zum bunten Papagei" };
+            Pirat pirat = new() { Name = "Guybrush" };
+            ozean.Island = insel;
+            insel.NaturContainer = wald;
+            insel.BarContainer = kneipe;
+            insel.BarContainer.Container = pirat;
+            insel.ZeigDichPirat();
+            ozean.SaveBinary();
+            ozean.SaveXML();
+            ozean.SaveJson();
+            insel.PiratContainer = pirat;
+            insel.BarContainer.Container = null;
+            insel.ZeigDichPirat();
 
             //ozean.LoadBinary();
             //ozean.LoadXML();
